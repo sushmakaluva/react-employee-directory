068ab172804cf7875213e9012527f23409d1c82e
Sushma Kaluva <kaluvasushma@gmail.com>
2020-10-16 16:40:24 -0400

    Updates

---
+++ precache-manifest.31e6281591722fef747bd0676e995dba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c763de35e2ff9e70c4bc33340973682e",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "4d61e55169b943d06fdb",
+    "url": "/react-employee-directory/static/css/main.0921df62.chunk.css"
+  },
+  {
+    "revision": "842c4c02e15a5c51244a",
+    "url": "/react-employee-directory/static/js/2.b8c67263.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-directory/static/js/2.b8c67263.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d61e55169b943d06fdb",
+    "url": "/react-employee-directory/static/js/main.30d61f49.chunk.js"
+  },
+  {
+    "revision": "193cdcbd01f85fb428f8",
+    "url": "/react-employee-directory/static/js/runtime-main.76415422.js"
+  }
+]);
